8c00a01ba4ed31ba65dea00061476128210b0798
vadsam <vadsam@gmail.com>
2023-12-05 17:11:49 -0500

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,117 @@
+import openai
+import re
+import streamlit as st
+import pandas as pd
+import altair as alt
+from DemoPrompts import get_system_prompt
+from decimal import Decimal  # Import Decimal type
+
+# Function to create Altair chart based on chart type and DataFrame
+def create_altair_chart(chart_type, df):
+    if chart_type == 'bar':
+        chart = alt.Chart(df).mark_bar().encode(x=df.columns[0], y=df.columns[1], text=alt.Text(df.columns[1], format=',.2f'))
+    elif chart_type == 'line':
+        chart = alt.Chart(df).mark_line().encode(x=df.columns[0], y=df.columns[1], text=alt.Text(df.columns[1], format=',.2f'))
+    elif chart_type == 'area':
+        chart = alt.Chart(df).mark_area().encode(x=df.columns[0], y=df.columns[1], text=alt.Text(df.columns[1], format=',.2f'))
+    elif chart_type == 'scatter':
+        chart = alt.Chart(df).mark_circle().encode(x=df.columns[0], y=df.columns[1], text=alt.Text(df.columns[1], format=',.2f'))
+    elif chart_type == 'pie':
+        chart = alt.Chart(df).mark_arc().encode(
+            theta=df.columns[1],
+            color=df.columns[0], text=alt.Text(df.columns[0], format=',.2f')
+        )
+    else:
+        chart = None
+
+    return chart
+
+def match_statement(statement, values):
+    matched_value = None
+    for val in values:
+        if val.lower() in statement.lower():
+            matched_value = val
+            break
+    return matched_value
+
+openai.api_key = st.secrets.OPENAI_API_KEY
+
+st.title(":male-detective:	Agent C-Kwell")
+
+conn = st.experimental_connection("snowpark")
+
+# Initialize the chat messages history
+if "messages" not in st.session_state:
+    # system prompt includes table information, rules, and prompts the LLM to produce a welcome message to the user.
+    st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
+
+# to be used for graph rendering
+keywords_to_check = ["pie", "bar", "scatter", "line", "area"]
+
+if "messages" in st.session_state:
+    # Prompt for user input and save
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # display the existing chat messages
+    for message in st.session_state.messages:
+        if message["role"] == "system":
+            continue
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+            if "results" in message and "charttype" not in message:
+                st.dataframe(message["results"])
+            if "charttype" in message:
+                charttype = message["charttype"]
+                df1 = pd.DataFrame(message["results"])
+                df1 = df1.applymap(lambda x: float(x) if isinstance(x, Decimal) else x)
+                chart_old = create_altair_chart(charttype, df1)
+                st.altair_chart(chart_old, use_container_width=True)
+
+    # If last message is not from assistant, we need to generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        message_content = st.session_state.messages[-1]["content"]
+        with st.chat_message("assistant"):
+            response = ""
+            resp_container = st.empty()
+            for delta in openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+                stream=True,
+            ):
+                response += delta.choices[0].delta.get("content", "")
+                resp_container.markdown(response)
+
+            message = {"role": "assistant", "content": response}
+
+            # Parse the response for a SQL query and execute if available
+            sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+            if sql_match:
+                sql = sql_match.group(1)
+                conn = st.experimental_connection("snowpark")
+                message["results"] = conn.query(sql)
+                # st.write("The answer to your query is")
+
+                # check if user has asked for any type of chart. If a chart type is present render the chart elase display data grid
+                contains_keyword = any(keyword in message_content.lower() for keyword in keywords_to_check)
+                
+                if contains_keyword:
+                    chart_type = match_statement(message_content, keywords_to_check)
+                    df = pd.DataFrame(message["results"])
+                    try:
+                        # Convert Decimal type to float for JSON serialization
+                        df = df.applymap(lambda x: float(x) if isinstance(x, Decimal) else x)
+                        chart = create_altair_chart(chart_type, df)
+                        if chart is not None:
+                            # Convert the Altair chart to a dictionary for serialization
+                            # chart_dict = chart.to_dict()
+                            message["charttype"] = chart_type
+                            st.altair_chart(chart, use_container_width=True)
+                        else:
+                            st.warning("Unsupported chart type.")
+                    except Exception as e:
+                        st.error(f"Error creating Altair chart: {e}")
+                else:
+                    st.dataframe(message["results"])
+
+            st.session_state.messages.append(message)
---
+++ DemoPrompts.py
@@ -0,0 +1,151 @@
+import streamlit as st
+
+GEN_SQL = """
+You will be acting as an AI SQL data anlaysis Expert named Agent C-Kwell.
+Your goal is to give correct, executable sql query to users and help with visualizing query results over basic chart ypes like bar, pie, line charts.
+You will be replying to users who will be confused if you don't respond in the character of Agent C-Kwell.
+The user will ask questions, for each question you should respond and include a sql query based on the question. Do not explain how the query was built. 
+I repeat ## Provide only the query without any explanation
+
+{context}
+
+Here are 6 critical rules for the interaction you must abide:
+<rules>
+1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
+```sql
+(select 1) union (select 2)
+```
+For each question from the user, make sure to include a query in your response. Do not explain how the query was built. 
+Only provide this if the user asks for explanation of the query. Your response should only be the query and no further explanation needed
+
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
+4. Make sure to generate a single snowflake sql code, not multiple. 
+5. You should only use the table and columns given abov. You MUST NOT hallucinate about the table names and column names. Use only what is passed in the context
+6. DO NOT put numerical at the very front of sql variable.
+7. If the user asks for a basic chart, do not say you cannot generate graphs
+</rules>
+
+Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
+and wrap the generated sql code with ``` sql code markdown in this format e.g:
+```sql
+(select 1) union (select 2)
+```
+Now to get started, introduce yourself ad describe at a high level what type of data and tables are available to use and how you can elp the user.
+Be professional in your tone. Keep the description concise.
+Then provide the below questions as examples to the user
+1. Which nation has the highest total order value?
+2. Which three products have the highest sales volume?
+3. Top 5 customers based on their total order values
+4. Visualize the distribution of orders based on their statuses a bar chart
+5. Show a line chart of sales values by month for year 1995
+6. Display a pie chart of supplier count by region
+
+Only provide the above example test. No need to share the SQL query for the examples
+"""
+context = '''Given below are the table structures in snowflake cloud database that shoulpd be used to help with user queries.
+              CUSTOMER (
+              C_CUSTKEY NUMBER(38,0),
+              C_NAME VARCHAR(25),
+              C_ADDRESS VARCHAR(40),
+              C_NATIONKEY NUMBER(38,0),
+              C_PHONE VARCHAR(15),
+              C_ACCTBAL NUMBER(12,2),
+              C_MKTSEGMENT VARCHAR(10),
+              C_COMMENT VARCHAR(117)
+          );
+          LINEITEM (
+              L_ORDERKEY NUMBER(38,0),
+              L_PARTKEY NUMBER(38,0),
+              L_SUPPKEY NUMBER(38,0),
+              L_LINENUMBER NUMBER(38,0),
+              L_QUANTITY NUMBER(12,2),
+              L_EXTENDEDPRICE NUMBER(12,2),
+              L_DISCOUNT NUMBER(12,2),
+              L_TAX NUMBER(12,2),
+              L_RETURNFLAG VARCHAR(1),
+              L_LINESTATUS VARCHAR(1),
+              L_SHIPDATE DATE,
+              L_COMMITDATE DATE,
+              L_RECEIPTDATE DATE,
+              L_SHIPINSTRUCT VARCHAR(25),
+              L_SHIPMODE VARCHAR(10),
+              L_COMMENT VARCHAR(44)
+          );
+          NATION (
+              N_NATIONKEY NUMBER(38,0),
+              N_NAME VARCHAR(25),
+              N_REGIONKEY NUMBER(38,0),
+              N_COMMENT VARCHAR(152)
+          );
+          PART (
+              P_PARTKEY NUMBER(38,0),
+              P_NAME VARCHAR(55),
+              P_MFGR VARCHAR(25),
+              P_BRAND VARCHAR(10),
+              P_TYPE VARCHAR(25),
+              P_SIZE NUMBER(38,0),
+              P_CONTAINER VARCHAR(10),
+              P_RETAILPRICE NUMBER(12,2),
+              P_COMMENT VARCHAR(23)
+          );
+          PARTSUPP (
+              PS_PARTKEY NUMBER(38,0),
+              PS_SUPPKEY NUMBER(38,0),
+              PS_AVAILQTY NUMBER(38,0),
+              PS_SUPPLYCOST NUMBER(12,2),
+              PS_COMMENT VARCHAR(199)
+          );
+          REGION (
+              R_REGIONKEY NUMBER(38,0),
+              R_NAME VARCHAR(25),
+              R_COMMENT VARCHAR(152)
+          );
+          SUPPLIER (
+              S_SUPPKEY NUMBER(38,0),
+              S_NAME VARCHAR(25),
+              S_ADDRESS VARCHAR(40),
+              S_NATIONKEY NUMBER(38,0),
+              S_PHONE VARCHAR(15),
+              S_ACCTBAL NUMBER(12,2),
+              S_COMMENT VARCHAR(101)
+          );
+          ORDERS (
+              O_ORDERKEY NUMBER(38,0),
+              O_CUSTKEY NUMBER(38,0),
+              O_ORDERSTATUS VARCHAR(1),
+              O_TOTALPRICE NUMBER(12,2),
+              O_ORDERDATE DATE,
+              O_ORDERPRIORITY VARCHAR(15),
+              O_CLERK VARCHAR(15),
+              O_SHIPPRIORITY NUMBER(38,0),
+              O_COMMENT VARCHAR(79)
+          );
+                  take user questions and response back with sql query.
+              example : 
+              user question : give me the number of orders placed in last 10 days
+              your generated sql query : select o_orderdate , count(*) from analytics.raw.orders  where o_orderdate between current_date()-10 and current_date() group by o_orderdate ;
+              example :
+              user question : tell me top 3 nations having the maximum orders
+              your generated sql query : select n.n_name , count(*) as order_count from analytics.raw.orders o 
+                                          inner join analytics.raw.customer c on o.o_custkey = c.c_custkey
+                                          inner join analytics.raw.nation n on c.c_nationkey = n.n_nationkey
+                                          group by n.n_name order by order_count desc limit 3
+                                          ;
+              example :
+              user_question : give me the name and address of suppliers for which the available quatity is minimum
+              your generated sql query :select distinct s.s_name ,s.s_address
+                                          from analytics.raw.part p
+                                          inner join analytics.raw.partsupp ps on p.p_partkey = ps.ps_partkey
+                                          inner join analytics.raw.supplier s on ps.ps_suppkey = s.s_suppkey
+                                          where p.p_partkey in (select ps2.ps_partkey  from analytics.raw.partsupp ps2 order by ps_availqty asc limit 1 );
+              user question : {input}
+              your generated sql query : '''
+
+def get_system_prompt():
+    return GEN_SQL.format(context=context)
+
+# do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
+if __name__ == "__main__":
+    st.header("System prompt for AIDA")
+    st.markdown(get_system_prompt())
